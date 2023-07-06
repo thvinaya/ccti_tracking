64a8db506f061c3606f9abb504c5a292d141a3ec
vinaya <vinaya@lzindia.co.in>
2023-07-06 11:23:32 +0530

    test

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -6,5 +6,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda-enterprise!')
+        'body': json.dumps('Hai from Lambda-enterprise!')
     }
